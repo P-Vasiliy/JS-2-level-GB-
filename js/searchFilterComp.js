1de195a1f0a287265e2731a5f199472b97354abb
Vasiliy Pavlushin <vasili_p@inbox.ru>
2022-10-23 11:24:01 +0300

    Исправления в файле поиска (путь к изображению лупы) по уроку №6 JS-Продвинутый курс

--- js/searchFilterComp.js
+++ js/searchFilterComp.js
@@ -21,7 +21,7 @@ Vue.component('search-filter', {
     <form action="#" class="header__search-form" @submit.prevent="search" @input="search">
           <input type="search" class="header__search-input" v-model="inputText">
           <button class="header__search-button" type="submit">
-              <img src="../img/loupe.svg" alt="search-icon">
+              <img src="./img/loupe.svg" alt="search-icon">
           </button>
     </form>`
 });
